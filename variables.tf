cf25998f90bb21fe9f41f7f3e55db21249f1b622
Trinath9395 <trinathdevops0606@gmail.com>
2025-09-08 14:50:08 +0530

    vpc

--- variables.tf
+++ variables.tf
@@ -23,3 +23,22 @@ variable "vpc_tags" {
     Purpose = "assignment"
   }
 }
+
+variable "public_subnet_cidr" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR blocks for private subnets"
+  type = list(string)
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "database_subnet_cidr" {
+  description = "CIDR blocks for private subnets"
+  type = list(string)
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
--- vpc.tf
+++ vpc.tf
@@ -5,4 +5,8 @@ module "vpc" {
   vpc_cidr = var.vpc_cidr
   common_tags = var.common_tags
   vpc_tags = var.vpc_tags
+  public_subnet_cidr = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+  database_subnet_cidr = var.database_subnet_cidr
+  is_peering_required = true 
 }
